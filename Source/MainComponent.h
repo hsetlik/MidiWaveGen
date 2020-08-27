a2240da9c6785afafbbcbd368ad5c03a082b93cf
Hayden Setlik <haydensetlik@gmail.com>
2020-08-27 10:59:00 -0600

    working with pure sine waves

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,32 +1,113 @@
+//
+//  MainComponent.h
+//  MidiWaveGen
+//
+//  Created by Hayden Setlik on 8/27/20.
+//
 #pragma once
-
 #include <JuceHeader.h>
+#include "SynthAudioSource.h"
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent   : public juce::AudioAppComponent,
+                               private juce::Timer
 {
 public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent() override;
+    MainComponent()
+        : synthAudioSource  (keyboardState),
+        keyboardComponent (keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard)
+    {
+        addAndMakeVisible (midiInputListLabel);
+        midiInputListLabel.setText ("MIDI Input:", juce::dontSendNotification);
+        midiInputListLabel.attachToComponent (&midiInputList, true);
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
+        auto midiInputs = juce::MidiInput::getAvailableDevices();
+        addAndMakeVisible (midiInputList);
+        midiInputList.setTextWhenNoChoicesAvailable ("No MIDI Inputs Enabled");
 
-    //==============================================================================
-    void paint (juce::Graphics& g) override;
-    void resized() override;
+        juce::StringArray midiInputNames;
+        for (auto input : midiInputs)
+            midiInputNames.add (input.name);
+
+        midiInputList.addItemList (midiInputNames, 1);
+        midiInputList.onChange = [this] { setMidiInput (midiInputList.getSelectedItemIndex()); };
+
+        for (auto input : midiInputs)
+        {
+        if (deviceManager.isMidiInputDeviceEnabled (input.identifier))
+            {
+                setMidiInput (midiInputs.indexOf (input));
+                break;
+            }
+        }
+        
+        if (midiInputList.getSelectedId() == 0)
+            setMidiInput (0);
+
+        addAndMakeVisible (keyboardComponent);
+        setAudioChannels (0, 2);
+        
+        setSize (600, 190);
+        startTimer (400);
+    }
+    
+    ~MainComponent() override
+    {
+        shutdownAudio();
+    }
+
+    void resized() override
+    {
+        midiInputList    .setBounds (200, 10, getWidth() - 210, 20);
+        keyboardComponent.setBounds (10,  40, getWidth() - 20, getHeight() - 50);
+    }
+
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
+    {
+        synthAudioSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
+    }
+
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
+    {
+        synthAudioSource.getNextAudioBlock (bufferToFill);
+    }
+
+    void releaseResources() override
+    {
+        synthAudioSource.releaseResources();
+    }
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+    void timerCallback() override
+    {
+        keyboardComponent.grabKeyboardFocus();
+        stopTimer();
+    }
 
+    void setMidiInput (int index)
+    {
+        auto list = juce::MidiInput::getAvailableDevices();
+
+        deviceManager.removeMidiInputDeviceCallback (list[lastInputIndex].identifier, synthAudioSource.getMidiCollector());
+
+        auto newInput = list[index];
+
+        if (! deviceManager.isMidiInputDeviceEnabled (newInput.identifier))
+            deviceManager.setMidiInputDeviceEnabled (newInput.identifier, true);
+
+        deviceManager.addMidiInputDeviceCallback (newInput.identifier, synthAudioSource.getMidiCollector());
+        midiInputList.setSelectedId (index + 1, juce::dontSendNotification);
+
+        lastInputIndex = index;
+    }
+
+    //==========================================================================
+    juce::MidiKeyboardState keyboardState;
+    SynthAudioSource synthAudioSource;
+    juce::MidiKeyboardComponent keyboardComponent;
+
+    juce::ComboBox midiInputList;
+    juce::Label midiInputListLabel;
+    int lastInputIndex = 0;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
---
+++ Source/SynthAudioSource.h
@@ -0,0 +1,60 @@
+//
+//  SynthAudioSource.h
+//  MidiWaveGen - App
+//
+//  Created by Hayden Setlik on 8/27/20.
+//
+#pragma once
+#include <JuceHeader.h>
+#include "SineWaveClasses.h"
+
+
+class SynthAudioSource   : public juce::AudioSource
+{
+public:
+    SynthAudioSource (juce::MidiKeyboardState& keyState)
+        : keyboardState (keyState)
+    {
+        for (auto i = 0; i < 4; ++i)
+            synth.addVoice (new SineWaveVoice());
+
+        synth.addSound (new SineWaveSound());
+    }
+
+    void setUsingSineWaveSound()
+    {
+        synth.clearSounds();
+    }
+
+    void prepareToPlay (int /*samplesPerBlockExpected*/, double sampleRate) override
+    {
+        synth.setCurrentPlaybackSampleRate (sampleRate);
+        midiCollector.reset (sampleRate); // [10]
+    }
+
+    void releaseResources() override {}
+
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
+    {
+        bufferToFill.clearActiveBufferRegion();
+
+        juce::MidiBuffer incomingMidi;
+        midiCollector.removeNextBlockOfMessages (incomingMidi, bufferToFill.numSamples); // [11]
+
+        keyboardState.processNextMidiBuffer (incomingMidi, bufferToFill.startSample,
+                                             bufferToFill.numSamples, true);
+
+        synth.renderNextBlock (*bufferToFill.buffer, incomingMidi,
+                               bufferToFill.startSample, bufferToFill.numSamples);
+    }
+
+    juce::MidiMessageCollector* getMidiCollector()
+    {
+        return &midiCollector;
+    }
+
+private:
+    juce::MidiKeyboardState& keyboardState;
+    juce::Synthesiser synth;
+    juce::MidiMessageCollector midiCollector;
+};
